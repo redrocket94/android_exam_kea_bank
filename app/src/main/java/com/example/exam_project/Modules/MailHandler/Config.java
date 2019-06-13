cc9d301140109f6deb86941fe298cc70ad01a654
Oliver Hegelund Duedam <49624019+redrocket94@users.noreply.github.com>
2019-06-13 12:43:37 +0200

    Update Config.java

--- app/src/main/java/com/example/exam_project/Modules/MailHandler/Config.java
+++ app/src/main/java/com/example/exam_project/Modules/MailHandler/Config.java
@@ -1,6 +1,6 @@
 package com.example.exam_project.Modules.MailHandler;
 
 public class Config {
-    public static final String EMAIL = "KEAbank.service@gmail.com";
-    public static final String PASSWORD = "keabank123";
+    public static final String EMAIL = ""; // CHANGE THIS TO YOUR SERVICE'S EMAIL
+    public static final String PASSWORD = ""; // CHANGE THIS TO YOUR SERVICE'S EMAIL'S PASSWORD
 }
